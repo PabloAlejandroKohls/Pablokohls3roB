9e83d52eea04f1c4bccf600000bc40d41f60b49c
PabloAlejandroKohls <50179203+PabloAlejandroKohls@users.noreply.github.com>
2019-05-01 12:52:13 -0500

    Add files via upload

---
+++ Tablamultiplicar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tablamultiplicar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //crear el programa tabla multiplicar que escriba la tabla de multiplicar del 1 al numero 15
+            int numero = 0, multiplicacion = 0;
+
+            Console.WriteLine("los resultados de la siguiente multiplicacion es : ");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int numero1= 1; numero1 <= 15; numero1++)
+            {
+                multiplicacion = numero * numero1;
+                Console.WriteLine(numero1 + "*" + numero + "=" + multiplicacion);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Trescolumnas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trescolumnas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //crear un programa que lea un numero entero y positivoy que esescriba tres columnas
+            int numeros0=0, numeros1 = 0, numeros2 = 0;
+            Console.WriteLine("Ingrese la cantidad de numeros de la serie: ");
+            int entero = Convert.ToInt16(Console.ReadLine());
+            for ( numeros0 = 0; numeros0 <= entero; numeros0++)
+            {
+                numeros1 = ((numeros0 + 2) + numeros0);
+                numeros2 = ((numeros0 + 3) + numeros0 + numeros0);
+                Console.WriteLine((numeros0) + "  " + "  " + "  " + (numeros1) + "  " + "  " + "  " + (numeros2));
+            }
+            Console.ReadKey();
+        }
+    }
+}
